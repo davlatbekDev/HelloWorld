5dc3c2eb0d2a19449bf79ec55faa8589b6b97256
davlatbekDev <amonovdavlatbek06@gmail.com>
2024-02-07 15:46:30 +0500

    hello world

---
+++ main.py
@@ -0,0 +1,29 @@
+#DT and DSA
+'''
+DT->(Data Type->MA`LUMOT TURLARI)
+DSA->(Data Structure)(Ma`lumotlar tuzilmasi)
+'''
+#DT
+'''
+int  - (integer)->(Butun sonlar) - 1,2,3,4,0,-3,-9
+float - (o`nli kasr ko`rinishidagi butun sonlar)- 3.14,2.71,9.81
+bool - (mantiq)->(True,False)
+#char - (belgi)->('A','#','1')->ASCII jadvalidagi belgilarni o`z ichiga oladi
+
+a = 13
+b = 12
+answer = a+b
+print(answer)
+#DSA
+'''
+'''
+string
+list
+dict
+tuple
+set
+map
+vector
+array
+'''
+print("Hello World")
